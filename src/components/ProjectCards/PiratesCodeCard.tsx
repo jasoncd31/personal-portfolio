399ad6ab4e65d5c5d17f21310296bfa09523253a
Jason Douglas <jasoncd31@gmail.com>
2023-12-19 14:23:18 -0800

    add pirates code logo to card

--- src/components/ProjectCards/PiratesCodeCard.tsx
+++ src/components/ProjectCards/PiratesCodeCard.tsx
@@ -5,6 +5,7 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import PirateLogo from '../../images/piratescodelogo.jpg';
 
 const useStyles = makeStyles({
   root: {
@@ -40,7 +41,7 @@ export default function DBCard() {
         <Typography variant="h5" color="textSecondary">
           Languages and Automata II Final
         </Typography>
-        <img src="https://placehold.co/600x400" alt="Cool Stuff" width="100%"/>
+        <img src={PirateLogo} alt="Cool Stuff" width="100%"/>
         <Typography variant="body2" component="p">
           PiratesCode was creating as a final project for my Languages and Automata II class. It is a pirate-themed programming language that compiles to JavaScript. It uses OHM to parse the code and generate the AST. It then uses the AST to generate JavaScript code. 
         </Typography>
